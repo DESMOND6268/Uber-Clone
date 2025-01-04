4e91cc9fc91f2e6a6f8a05fe677ff5d0cc4be3aa
DESMOND6268 <rishisinghrathod2003@gmail.com>
2025-01-04 15:17:38 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,20 @@
 const dotenv = require('dotenv');
-dotenv.config()
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes'); // Fix the typo here
 
+connectToDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Welcome to my App!');
-})
+});
+app.use('/users', userRoutes); // Fix the typo here
 
 module.exports = app;
