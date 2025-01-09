bb2e57a355b4b84fa19db9c7d821e0708b66ef63
DESMOND6268 <rishisinghrathod2003@gmail.com>
2025-01-09 22:30:59 +0530

    captain registrations done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const captainModel = require('../models/captain.model')
 
 module.exports.authUser = async (req, res, next) => {
     try {
@@ -8,15 +10,15 @@ module.exports.authUser = async (req, res, next) => {
 
         // Check if the token exists
         if (!token) {
-            return res.status(401).json({ message: 'Unauthorized Access: No Token Provided' });
+            return res.status(401).json({ message: 'Unauthorized Access' });
         }
 
-        const isBlacklisted = await userModel.findOne({ token: token});
+        const isBlacklisted = await blacklistTokenModel.findOne({ token: token});
 
         if (isBlacklisted) {
             return res.status(401).json({ message: 'Unauthorized Access' });
         }
-        
+
         // Verify the token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
@@ -34,3 +36,27 @@ module.exports.authUser = async (req, res, next) => {
         return res.status(401).json({ message: 'Unauthorized Access: Invalid or Expired Token', error: error.message });
     }
 }; 
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized Access' });
+    }
+
+    const isBlacklisted = await blacklistTokenModel.findOne({ token: token});
+    
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized Access' });
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        req.captain = captain;
+        return next()
+    }catch (err) {
+        res.status(401).json({ message: 'Unauthorized Access'});
+     }
+};
